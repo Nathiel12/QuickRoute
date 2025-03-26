7a36e3618344f9f343e6483ea188d3df880e1c0d
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:34:54 +0100

    Creado modelo traslados

--- QuickRoute/QuickRoute/Data/Models/Traslados.cs
+++ QuickRoute/QuickRoute/Data/Models/Traslados.cs
@@ -1,6 +1,24 @@
-﻿namespace QuickRoute.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
 {
     public class Traslados
     {
+        [Key]
+        public int TrasladoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public bool? Aprobado { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public Carros Carro { get; set; }
+        public int CarroId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "Ingrese un precio valido")]
+        public double Monto { get; set; }
+        public string Id { get; set; } 
+        public ApplicationUser Usuario { get; set; }
+
     }
 }
