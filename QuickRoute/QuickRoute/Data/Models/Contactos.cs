812d19c7ba938b9d61de7fb9004c1d7d1512d1d8
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 20:20:24 +0100

    Creacion del modelo contactos

---
+++ QuickRoute/QuickRoute/Data/Models/Contactos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
+{
+    public class Contactos
+    {
+        [Key]
+        public int ContactoId { get; set; }
+        [Required]
+        [Phone]
+        public string Telefono { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+    }
+}
