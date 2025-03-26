17f97a58a703d46ab1d03b4190072e404a3e064b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:33:18 +0100

    Creado modelo declaraciones

--- QuickRoute/QuickRoute/Data/Models/Declaraciones.cs
+++ QuickRoute/QuickRoute/Data/Models/Declaraciones.cs
@@ -1,6 +1,28 @@
-﻿namespace QuickRoute.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
 {
     public class Declaraciones
     {
+        [Key]
+        public int DeclaracionId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El nombre solo puede contener letras y espacios")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime FechaEntrega { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "Ingrese un precio valido")]
+        public double ValorMercancia { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "Ingrese un precio valido")]
+        public double ValorTransportacion { get; set; }
     }
 }
