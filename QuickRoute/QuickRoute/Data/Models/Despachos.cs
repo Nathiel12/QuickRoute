4b743794de65c6503b7e69e78edb001b1f2ff39e
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:34:32 +0100

    Creado modelo despachos

--- QuickRoute/QuickRoute/Data/Models/Despachos.cs
+++ QuickRoute/QuickRoute/Data/Models/Despachos.cs
@@ -1,6 +1,19 @@
-﻿namespace QuickRoute.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
 {
     public class Despachos
     {
+        [Key]
+        public int SolicitudId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Localizacion { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime FechaSolicitud { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string NombreEmpresa { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Titulo { get; set; }
+        public IList<Impuestos> Impuestos { get; set; } = new List<Impuestos>();
     }
 }
