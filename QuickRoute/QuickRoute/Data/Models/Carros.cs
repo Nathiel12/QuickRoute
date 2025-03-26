c732cf5ccb431c71147126eb730e6d5d447f5669
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 20:19:59 +0100

    Creacion de modelo carros

---
+++ QuickRoute/QuickRoute/Data/Models/Carros.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
+{
+    public class Carros
+    {
+        [Key]
+        public int CarroId { get; set; }
+
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime FechaFabricacion { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string Color { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(0, double.MaxValue, ErrorMessage ="Ingrese un precio valido")]
+        public double Precio { get; set; }
+
+        [Required(ErrorMessage = "El número de chasis es obligatorio.")]
+        [StringLength(17, MinimumLength = 17, ErrorMessage = "El número de chasis debe tener exactamente 17 caracteres.")]
+        [RegularExpression(@"^[A-HJ-NPR-Z0-9]{17}$", ErrorMessage = "El número de chasis contiene caracteres inválidos. (No debe contener I u O")]
+        public string NumeroChasis { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public byte[] Factura { get; set; }
+    }
+}
