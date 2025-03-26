a8ca032c460c71ab03757e5587368538e4e55d1a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:43:23 +0100

    Agregado restricciones

--- QuickRoute/QuickRoute/Data/Models/Despachos.cs
+++ QuickRoute/QuickRoute/Data/Models/Despachos.cs
@@ -6,14 +6,24 @@ namespace QuickRoute.Data.Models
     {
         [Key]
         public int SolicitudId { get; set; }
+
         [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(255, ErrorMessage = "La localización no puede superar los 255 caracteres")]
         public string Localizacion { get; set; }
+
         [Required(ErrorMessage = "Este campo es requerido")]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "01/01/1900", "01/01/2100", ErrorMessage = "La fecha debe estar entre 01/01/1900 y 01/01/2100")]
         public DateTime FechaSolicitud { get; set; } = DateTime.Now;
+
         [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(200, ErrorMessage = "El nombre de la empresa no puede superar los 200 caracteres")]
         public string NombreEmpresa { get; set; }
+
         [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(100, ErrorMessage = "El título no puede superar los 100 caracteres")]
         public string Titulo { get; set; }
+
         public IList<Impuestos> Impuestos { get; set; } = new List<Impuestos>();
     }
 }
