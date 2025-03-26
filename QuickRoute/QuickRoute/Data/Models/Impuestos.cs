d61774d59a1e221febd9ab6b8efa61d4e207713b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:30:34 +0100

    Creado modelo para impuestos

---
+++ QuickRoute/QuickRoute/Data/Models/Impuestos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuickRoute.Data.Models
+{
+    public class Impuestos
+    {
+        [Key]
+        public int ImpuestoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, double.MaxValue, ErrorMessage = "Ingrese un precio valido")]
+        public double Monto { get; set; }
+        public int SolicitudId { get; set; }
+        [ForeignKey("SolicitudId")]
+        public Despachos Despacho { get; set; }
+    }
+}
