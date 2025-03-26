cd2c20b10777b90b7839a75eeca1f91c8e7ffe01
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-26 22:58:15 +0100

    Agregado relaciones a ApplicationUser

--- QuickRoute/QuickRoute/Data/ApplicationUser.cs
+++ QuickRoute/QuickRoute/Data/ApplicationUser.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Identity;
+using QuickRoute.Data.Models;
 
 namespace QuickRoute.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public ICollection<Traslados> Traslados { get; set; }
+        public ICollection<Despachos> Despachos { get; set; }
+        public ICollection<Declaraciones> Declaraciones { get; set; }
         public required string Role { get; set; }
     }
 
