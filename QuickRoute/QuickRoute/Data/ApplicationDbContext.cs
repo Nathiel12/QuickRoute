b128bcc4100951590fb497267ffde25133e8a5bc
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-03-31 04:31:01 +0200

    Agregado on model creating para impuestos

--- QuickRoute/QuickRoute/Data/ApplicationDbContext.cs
+++ QuickRoute/QuickRoute/Data/ApplicationDbContext.cs
@@ -12,5 +12,18 @@ namespace QuickRoute.Data
         public virtual DbSet<Despachos> Despachos { get; set; }
         public virtual DbSet<Impuestos> Impuestos { get; set; }
         public virtual DbSet<Traslados> Traslados { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Impuestos>().HasData(
+                new Impuestos { ImpuestoId = 1, Nombre = "Tasa por Servicio Aduanero", Monto = 0 },
+                new Impuestos { ImpuestoId = 2, Nombre = "Arancelarios", Monto = 0 },
+                new Impuestos { ImpuestoId = 3, Nombre = "ITBIS", Monto = 0 },
+                new Impuestos { ImpuestoId = 4, Nombre = "Declaracion del Valor", Monto = 0 },
+                new Impuestos { ImpuestoId = 5, Nombre = "Recargos por Declaracion Tardia", Monto = 0 },
+                new Impuestos { ImpuestoId = 6, Nombre = "Declaracion Unica Aduanera", Monto = 0 }
+            );
+        }
+
     }
 }
