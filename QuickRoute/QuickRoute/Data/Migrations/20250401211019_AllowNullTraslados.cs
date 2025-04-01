7ced163fe66af8fda837ce23c4ab8156878a9983
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-02 01:45:45 +0200

    Migracion para los traslados nullos

---
+++ QuickRoute/QuickRoute/Data/Migrations/20250401211019_AllowNullTraslados.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuickRoute.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowNullTraslados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carros_Traslados_TrasladoId",
+                table: "Carros");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrasladoId",
+                table: "Carros",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carros_Traslados_TrasladoId",
+                table: "Carros",
+                column: "TrasladoId",
+                principalTable: "Traslados",
+                principalColumn: "TrasladoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carros_Traslados_TrasladoId",
+                table: "Carros");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TrasladoId",
+                table: "Carros",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carros_Traslados_TrasladoId",
+                table: "Carros",
+                column: "TrasladoId",
+                principalTable: "Traslados",
+                principalColumn: "TrasladoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
