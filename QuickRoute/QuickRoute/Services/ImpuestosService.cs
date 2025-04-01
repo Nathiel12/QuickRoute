832148dc821c91d27c54dcd2b15a8bd71dfa1323
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-02 01:46:17 +0200

    Agregado metodos para buscar y listar

--- QuickRoute/QuickRoute/Services/ImpuestosService.cs
+++ QuickRoute/QuickRoute/Services/ImpuestosService.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using QuickRoute.Data;
+using QuickRoute.Data.Models;
 
 namespace QuickRoute.Services
 {
     public class ImpuestosService(IDbContextFactory<ApplicationDbContext> DbFactory)
     {
+        public async Task<List<Impuestos>> Listar(Expression<Func<Impuestos, bool>> criterio)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Impuestos
+                .Where(criterio)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        public async Task<Impuestos> Buscar(int impuestoId)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            return await contexto.Impuestos
+                .AsNoTracking()
+                .FirstOrDefaultAsync(i => i.ImpuestoId == impuestoId);
+        }
     }
 }
