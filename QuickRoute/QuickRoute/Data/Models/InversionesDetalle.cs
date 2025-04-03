a104543287cdb18186b48ab91dc3ba558008711a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-03 23:25:44 +0200

    Creado modelo inversiones detalle

---
+++ QuickRoute/QuickRoute/Data/Models/InversionesDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuickRoute.Data.Models
+{
+    public class InversionesDetalle
+    {
+        [Key]
+        public int InversionDetalleId { get; set; }
+        public int TipoVehiclo { get; set; }
+
+        [Range(0.0000000, double.MaxValue, ErrorMessage = "El monto debe ser positivo")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public double Monto { get; set; }
+
+        [ForeignKey("TipoVehiculoId")] 
+        public TipoVehiculos TipoVehiculo { get; set; } = null!;
+        public int InversionId { get; set; }
+
+        [ForeignKey(nameof(InversionId))] 
+        public Inversiones Inversion { get; set; } = null!;
+    }
+}
