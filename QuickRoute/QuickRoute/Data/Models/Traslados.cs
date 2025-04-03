747735bb9363542392d49790e19b36290ea689c2
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-03 23:26:44 +0200

    Eliminar propiedad de aprobado

--- QuickRoute/QuickRoute/Data/Models/Traslados.cs
+++ QuickRoute/QuickRoute/Data/Models/Traslados.cs
@@ -8,14 +8,12 @@ namespace QuickRoute.Data.Models
         [Key]
         public int TrasladoId { get; set; }
 
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public bool? Aprobado { get; set; }
-
         public List<TrasladosDetalle> TrasladosDetalles { get; set; } = new List<TrasladosDetalle>();
 
         public string Id { get; set; }
 
         [ForeignKey("Id")]
         public ApplicationUser Usuario { get; set; }
+        public ICollection<Carros> Carros { get; set; }
     }
 }
