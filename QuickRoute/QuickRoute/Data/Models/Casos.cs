89497d9463b5c5d2ee702d544edecb77a2b29415
VSFerreiras <VSFerreiras402@protonmail.com>
2025-04-05 23:22:55 -0700

    Modelo de casos corregido

--- QuickRoute/QuickRoute/Data/Models/Casos.cs
+++ QuickRoute/QuickRoute/Data/Models/Casos.cs
@@ -5,22 +5,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Casos
 {
-    [Key] 
+    [Key]
     public int CasoId { get; set; }
 
-    public int ContactoId { get; set; }
-    
     [Required(ErrorMessage = "Por favor, elija un contacto aduanero")]
-    public Contactos Contacto { get; set; }
+    public int ContactoId { get; set; }  
+
+    [ForeignKey("ContactoId")]
+    public Contactos? Contacto { get; set; }  
 
     public DateTime Fecha { get; set; } = DateTime.Today;
-    
-    [Required(ErrorMessage = "Este campo es requerido")] 
-    [StringLength(50, ErrorMessage = "El asusnto no puede exceder los 50 caracteres")] 
-    public string Asunto { get; set; }
 
-    [Required(ErrorMessage = "Este campo es requerido")] 
-    [StringLength(250, ErrorMessage = "La descripción no puede exceder los 250 caracteres")] 
-    public string Descripcion { get; set; }
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [StringLength(50, ErrorMessage = "El asunto no puede exceder los 50 caracteres")]
+    public string Asunto { get; set; } = string.Empty;  
 
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [StringLength(250, ErrorMessage = "La descripción no puede exceder los 250 caracteres")]
+    public string Descripcion { get; set; } = string.Empty;  
 }
