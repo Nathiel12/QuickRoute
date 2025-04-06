f70408b836bd4cc6ddfd18e5c42681756fdb0019
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-06 08:29:50 +0200

    Agregado restricciones

--- QuickRoute/QuickRoute/Data/Models/Sugerencias.cs
+++ QuickRoute/QuickRoute/Data/Models/Sugerencias.cs
@@ -11,11 +11,11 @@ public class Sugerencias
     public DateTime Fecha { get; set; } = DateTime.Today;
 
     [Required(ErrorMessage = "Este campo es requerido")]
-    [StringLength(50, ErrorMessage = "El asusnto no puede exceder los 50 caracteres")]
+    [StringLength(50, ErrorMessage = "El asunto no puede exceder los 50 caracteres")]
     public string Asunto { get; set; }
 
     [Required(ErrorMessage = "Este campo es requerido")]
-    [StringLength(250, ErrorMessage = "La descripción no puede exceder los 250 caracteres")]
+    [StringLength(250, ErrorMessage = "La descripcion no puede exceder los 250 caracteres")]
     public string Descripcion { get; set; }
 
     [Range(1,5)]
