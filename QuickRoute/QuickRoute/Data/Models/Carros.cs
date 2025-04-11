2c4868b28fef0a669939db23ad26ae168c051d7e
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-11 22:22:43 +0200

    Rehauled the model

--- QuickRoute/QuickRoute/Data/Models/Carros.cs
+++ QuickRoute/QuickRoute/Data/Models/Carros.cs
@@ -7,47 +7,17 @@ namespace QuickRoute.Data.Models
     {
         [Key]
         public int CarroId { get; set; }
-
-        [Required(ErrorMessage ="Este campo es requerido")]
-        [StringLength(15, ErrorMessage = "La marca no puede exceder los 15 caracteres")]
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "La marca solo puede contener letras y espacios")]
         public string Marca { get; set; }
 
-        public DateTime FechaRegistro { get; set; } = DateTime.Now;
-
-        [Required(ErrorMessage = "Este campo es requerido")]
-        [StringLength(15, ErrorMessage = "El modelo no puede exceder los 15 caracteres")]
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ0-9\s]+$",
-        ErrorMessage = "Solo se permiten letras, números y espacios (sin puntos ni símbolos)")]
         public string Modelo { get; set; }
-
-        [Required(ErrorMessage = "Este campo es requerido.")]
-        [Range(typeof(DateTime), "1/1/1900", "1/1/2100", ErrorMessage = "Fecha inválida")]
         public DateTime FechaFabricacion { get; set; } = DateTime.Now;
 
-        [Required(ErrorMessage = "Este campo es requerido")]
-        [StringLength(15, ErrorMessage = "El color no puede exceder los 15 caracteres.")]
-        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El color solo puede contener letras y espacios.")]
         public string Color { get; set; }
 
-        [Required(ErrorMessage = "Este campo es requerido.")]
-        [Range(1, 5000000, ErrorMessage = "Debe ser entre $1 y $5,000,000")]
         public double Precio { get; set; }
-
-        [Required(ErrorMessage = "El número de título es obligatorio.")]
-        [StringLength(17, MinimumLength = 17, ErrorMessage = "El número de título debe tener exactamente 17 caracteres.")]
-        [RegularExpression(@"^[A-HJ-NPR-Z0-9]{17}$", ErrorMessage = "El número de título contiene caracteres inválidos. (No debe contener I u O")]
         public string NumeroTitulo { get; set; }
-
-        [Required(ErrorMessage = "Este campo es requerido")]
-        public bool Aprobado { get; set; } = false;
-        public bool EnTraslado { get; set; } = false;
-        public int? TrasladoId { get; set; }
-        public Traslados? Traslado { get; set; }
-        public string Id { get; set; }
-        [ForeignKey("Id")]
-        public ApplicationUser Usuario { get; set; }
-        [NotMapped] 
-        public double MontoAcumuladoTraslados { get; set; }
+        public int CantidadStock { get; set; }
+        public string ImagenUrl { get; set; }
+        public bool Disponibilidad { get; set; } = true; 
     }
 }
