4679a6e0e5a5da65e62566a9846347d87596c856
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-11 23:55:11 +0200

    Creado modelo direccion

---
+++ QuickRoute/QuickRoute/Data/Models/Direccion.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuickRoute.Data.Models
+{
+    public class Direccion
+    {
+        [Key]
+        public int DireccionId { get; set; }
+        [Required]
+        public string Provincia { get; set; }
+        [Required]
+        public string Ciudad { get; set; }
+        [Required]
+        public string Direccion1 { get; set; }
+        public string Direccion2 { get; set; }
+        [Required]
+        public string CodigoPostal { get; set; }
+        public string Id { get; set; }
+        [ForeignKey("Id")]
+        public virtual ApplicationUser Usuario { get; set; }
+    }
+}
