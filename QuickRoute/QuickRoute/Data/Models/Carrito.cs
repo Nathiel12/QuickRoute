1ca5c4d176428c0190e869ee95e5ba0fb698156b
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-11 22:22:35 +0200

    Created carrito model

---
+++ QuickRoute/QuickRoute/Data/Models/Carrito.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuickRoute.Data.Models
+{
+    public class Carrito
+    {
+        public int CarritoId { get; set; }
+        public string Id { get; set; } 
+        public int CarroId { get; set; }     
+        public int Cantidad { get; set; } = 1;
+        public DateTime FechaAgregado { get; set; } = DateTime.Now;
+        public virtual Carros Carro { get; set; }
+        [ForeignKey("Id")]
+        public virtual ApplicationUser Usuario { get; set; }
+    }
+}
