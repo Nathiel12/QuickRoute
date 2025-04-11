146d293122ba8e69e2fb7879c8c556f80b13951a
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-11 23:55:46 +0200

    Corregido nombre

---
+++ QuickRoute/QuickRoute/Data/Models/Ordenes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuickRoute.Data.Models
+{
+    public class Ordenes
+    {
+        public int OrdenId { get; set; }
+        public string Id { get; set; }
+        public DateTime FechaOrden { get; set; } = DateTime.Now;
+        public double Total { get; set; }
+        public string Estado { get; set; } = "Pendiente";
+        public ICollection<OrdenDetalle> Detalles { get; set; }
+        [ForeignKey("Id")]
+        public virtual ApplicationUser Usuario { get; set; }
+    }
+}
