a3488462de4858f625d600b6bb97f9f7a1c8ca91
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-04-19 02:01:02 +0200

    Agregado restricciones

--- QuickRoute/QuickRoute/Data/Models/Carros.cs
+++ QuickRoute/QuickRoute/Data/Models/Carros.cs
@@ -7,14 +7,30 @@ namespace QuickRoute.Data.Models
     {
         [Key]
         public int CarroId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
         public string Marca { get; set; }
 
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(15, ErrorMessage = "El modelo no puede exceder los 15 caracteres")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ0-9\s]+$",
+        ErrorMessage = "Solo se permiten letras, números y espacios (sin puntos ni símbolos)")]
         public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Range(typeof(DateTime), "1/1/1886", "1/1/2100", ErrorMessage = "Fecha inválida")]
         public DateTime FechaFabricacion { get; set; } = DateTime.Now;
 
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(15, ErrorMessage = "El color no puede exceder los 15 caracteres.")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El color solo puede contener letras y espacios.")]
         public string Color { get; set; }
 
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [Range(1, 30000000, ErrorMessage = "Debe ser entre $1 y $30,000,000")]
         public double Precio { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [Range(1, 99, ErrorMessage = "Debe ser entre 1 y 99")]
         public int CantidadStock { get; set; }
         public string ImagenUrl { get; set; } = "/img/carros/default.jpg";
         public bool Disponibilidad { get; set; } = true; 
