74a3104824fa50219c90be4cc84a1f266b1782ff
Victor S. Ferreiras <vsferreiras402@protonmail.com>
2025-04-20 18:55:18 -0400

    removido traslados de ApplicationUser.cs

--- QuickRoute/QuickRoute/Data/ApplicationUser.cs
+++ QuickRoute/QuickRoute/Data/ApplicationUser.cs
@@ -6,7 +6,6 @@ namespace QuickRoute.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public ICollection<Traslados> Traslados { get; set; }
         public ICollection<Carros?> Carros { get; set; }
     }
 
